59fb6d3e397f698816e591c85a52362e2ba0c0e7
TKimura416 <kimuratakuya416@gmail.com>
2014-06-01 15:13:59 +0300

    Rename compound pk pg sequences

--- fixtures/x-relationships-compound/pg.sql
+++ fixtures/x-relationships-compound/pg.sql
@@ -43,11 +43,11 @@ ALTER TABLE "mto2" DROP CONSTRAINT "tbl_id";
 -- Table "otm1"
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS "otm1" ;
-CREATE SEQUENCE otm1_id1_seq;
-ALTER SEQUENCE otm1_id1_seq owner TO liolio;
+CREATE SEQUENCE otm1_id_seq;
+ALTER SEQUENCE otm1_id_seq owner TO liolio;
 
 CREATE TABLE IF NOT EXISTS "otm1" (
-  "id1" INT NOT NULL DEFAULT nextval('otm1_id1_seq'),
+  "id1" INT NOT NULL DEFAULT nextval('otm1_id_seq'),
   "id2" VARCHAR(45) NOT NULL,
   "name1" VARCHAR(45) NOT NULL,
   PRIMARY KEY ("id1", "id2"));
@@ -57,11 +57,11 @@ CREATE TABLE IF NOT EXISTS "otm1" (
 -- Table "otm2"
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS "otm2" ;
-CREATE SEQUENCE otm2_id1_seq;
-ALTER SEQUENCE otm2_id1_seq owner TO liolio;
+CREATE SEQUENCE otm2_id_seq;
+ALTER SEQUENCE otm2_id_seq owner TO liolio;
 
 CREATE TABLE IF NOT EXISTS "otm2" (
-  "id1" INT NOT NULL DEFAULT nextval('otm2_id1_seq'),
+  "id1" INT NOT NULL DEFAULT nextval('otm2_id_seq'),
   "id2" VARCHAR(45) NOT NULL,
   "name1" VARCHAR(45) NOT NULL,
   "name2" INT NOT NULL,
@@ -72,11 +72,11 @@ CREATE TABLE IF NOT EXISTS "otm2" (
 -- Table "mtm1"
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS "mtm1" ;
-CREATE SEQUENCE mtm1_id1_seq;
-ALTER SEQUENCE mtm1_id1_seq owner TO liolio;
+CREATE SEQUENCE mtm1_id_seq;
+ALTER SEQUENCE mtm1_id_seq owner TO liolio;
 
 CREATE TABLE IF NOT EXISTS "mtm1" (
-  "id1" INT NOT NULL DEFAULT nextval('mtm1_id1_seq'),
+  "id1" INT NOT NULL DEFAULT nextval('mtm1_id_seq'),
   "id2" VARCHAR(45) NOT NULL,
   "name1" VARCHAR(45) NOT NULL,
   PRIMARY KEY ("id1", "id2"));
@@ -86,11 +86,11 @@ CREATE TABLE IF NOT EXISTS "mtm1" (
 -- Table "mtm2"
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS "mtm2" ;
-CREATE SEQUENCE mtm2_id1_seq;
-ALTER SEQUENCE mtm2_id1_seq owner TO liolio;
+CREATE SEQUENCE mtm2_id_seq;
+ALTER SEQUENCE mtm2_id_seq owner TO liolio;
 
 CREATE TABLE IF NOT EXISTS "mtm2" (
-  "id1" INT NOT NULL DEFAULT nextval('mtm2_id1_seq'),
+  "id1" INT NOT NULL DEFAULT nextval('mtm2_id_seq'),
   "id2" VARCHAR(45) NOT NULL,
   "name1" VARCHAR(45) NOT NULL,
   "name2" INT NOT NULL,
@@ -101,11 +101,11 @@ CREATE TABLE IF NOT EXISTS "mtm2" (
 -- Table "tbl"
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS "tbl" ;
-CREATE SEQUENCE tbl_id1_seq;
-ALTER SEQUENCE tbl_id1_seq owner TO liolio;
+CREATE SEQUENCE tbl_id_seq;
+ALTER SEQUENCE tbl_id_seq owner TO liolio;
 
 CREATE TABLE IF NOT EXISTS "tbl" (
-  "id1" INT NOT NULL DEFAULT nextval('tbl_id1_seq'),
+  "id1" INT NOT NULL DEFAULT nextval('tbl_id_seq'),
   "id2" VARCHAR(45) NOT NULL,
   "otm1_id1" INT NOT NULL,
   "otm1_id2" VARCHAR(45) NOT NULL,
@@ -129,11 +129,11 @@ CREATE TABLE IF NOT EXISTS "tbl" (
 -- Table "mto1"
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS "mto1" ;
-CREATE SEQUENCE mto1_id1_seq;
-ALTER SEQUENCE mto1_id1_seq owner TO liolio;
+CREATE SEQUENCE mto1_id_seq;
+ALTER SEQUENCE mto1_id_seq owner TO liolio;
 
 CREATE TABLE IF NOT EXISTS "mto1" (
-  "id1" INT NOT NULL DEFAULT nextval('mto1_id1_seq'),
+  "id1" INT NOT NULL DEFAULT nextval('mto1_id_seq'),
   "id2" VARCHAR(45) NOT NULL,
   "tbl_id1" INT NOT NULL,
   "tbl_id2" VARCHAR(45) NOT NULL,
@@ -164,11 +164,11 @@ CREATE TABLE IF NOT EXISTS "mto1" (
 -- Table "mto2"
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS "mto2" ;
-CREATE SEQUENCE mto2_id1_seq;
-ALTER SEQUENCE mto2_id1_seq owner TO liolio;
+CREATE SEQUENCE mto2_id_seq;
+ALTER SEQUENCE mto2_id_seq owner TO liolio;
 
 CREATE TABLE IF NOT EXISTS "mto2" (
-  "id1" INT NOT NULL DEFAULT nextval('mto2_id1_seq'),
+  "id1" INT NOT NULL DEFAULT nextval('mto2_id_seq'),
   "id2" VARCHAR(45) NOT NULL,
   "tbl_id1" INT NOT NULL,
   "tbl_id2" VARCHAR(45) NOT NULL,
