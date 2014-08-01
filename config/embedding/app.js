b793b628eb1109a9f0f63cd72228158d7e25df13
TKimura416 <kimuratakuya416@gmail.com>
2014-08-01 19:59:23 +0300

    Use init shortcut in embed config

--- config/embedding/app.js
+++ config/embedding/app.js
@@ -3,25 +3,20 @@ var path = require('path');
 var express = require('express');
 
 
-var expressAdmin = require('/home/mighty/node/modules/express-admin');
+var xAdmin = require('express-admin');
 var dpath = path.resolve(__dirname, '../', process.argv[2], process.argv[3]);
 
-var expressAdminArgs = {
+var args = {
     dpath: dpath,
     config: require(path.join(dpath, 'config.json')),
     settings: require(path.join(dpath, 'settings.json')),
     custom: require(path.join(dpath, 'custom.json')),
     users: require(path.join(dpath, 'users.json'))
 };
-expressAdminArgs.config.app.root = '/admin';
+args.config.app.root = '/admin';
 
-expressAdmin.initDatabase(expressAdminArgs, function (err) {
+xAdmin.init(args, function (err, admin) {
     if (err) return console.log(err);
-
-    expressAdmin.initSettings(expressAdminArgs);
-
-    var admin = expressAdmin.initServer(expressAdminArgs);
-    
 
     // express 3.4.4
     var app = express()
